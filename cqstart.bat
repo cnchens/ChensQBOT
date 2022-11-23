250aa49421f8b562065bf16068a853016c45601e
Chens <1836178267@qq.com>
2022-11-23 19:16:16 +0800

    Update cqstart.bat

--- cqstart.bat
+++ cqstart.bat
@@ -1,1 +1,1 @@
-%~dp0\cqhttp_win_x86_64\ -c %~dp0\cqhttp_win_x86_64\config.yml -w %~dp0\cqhttp_win_x86_64\
+%~dp0\cqhttp_win_x86_64\go-cqhttp_windows_amd64.exe -c %~dp0\cqhttp_win_x86_64\config.yml -w %~dp0\cqhttp_win_x86_64\
