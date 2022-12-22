68d66840398c187d21ae17025b24991625f8aff5
Chens <1836178267@qq.com>
2022-12-22 15:55:35 +0800

    更新

--- static/js/start.js
+++ static/js/start.js
@@ -1,34 +1,9 @@
-$(function () {
+document.getElementById("section-1").onclick=function () {change_sc1()};
+function change_sc1() {
+    document.getElementById("text_iframe").style.src="https://cnchens.github.io/ChensBOT/templates/startt.html"
+}
 
-    //隐藏所有子标题
-    $(".nav-menu").each(function () {
-        $(this).children(".nav-content").hide();
-    });
-
-    //给菜单项中的所有主标题绑定事件
-    $(".nav-title").each(function () {
-        //获取点击当前标签下所有子标签
-        var navConEle = $(this).parents(".nav-menu").children(".nav-content");
-
-        //绑定点击事件,判断navConEle的display这个属性是否为none,时none的话时隐藏状态
-        $(this).click(function(){
-            if (navConEle.css("display") != "none") {
-                //隐藏,传参数自带动画,单位为毫秒
-                navConEle.hide(300);
-            } else {
-                //显示,传参数自带动画,单位为毫秒
-                $(".nav-menu").each(function () {
-                    $(this).children(".nav-content").hide(300);
-                });
-                navConEle.show(300);
-            }
-        });
-    });
-
-    $(".nav-content>a").each(function () {
-        $(this).click(function () {
-            $(".content-right").html($(this).html());
-        });
-    });
-
-});
+document.getElementById("section-2").onclick=function () {change_sc2()};
+function change_sc2() {
+    document.getElementById("text_iframe").style.src="https://cnchens.github.io/ChensBOT/templates/test.html"
+}
