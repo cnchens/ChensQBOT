5e96ec6b4afa70e61c9ad44bae4460dde7bedefa
Chens <1836178267@qq.com>
2022-12-22 12:24:09 +0800

    更新

---
+++ static/js/start.js
@@ -0,0 +1,23 @@
+$(function () {
+    $("#myButtons1 .btn").click(function(){
+        $(this).button('toggle');
+    });
+});
+
+$(function () {
+    $("#myButtons2 .btn").click(function(){
+        $(this).button('toggle');
+    });
+});
+
+$(function () {
+    $("#myButtons3 .btn").click(function(){
+        $(this).button('toggle');
+    });
+});
+
+$(function () {
+    $("#myButtons4 .btn").click(function(){
+        $(this).button('toggle');
+    });
+});
