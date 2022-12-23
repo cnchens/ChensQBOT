503e2d5ddaff925fdde8aafd875c651eee1c118e
Chens <1836178267@qq.com>
2022-12-23 12:30:22 +0800

    更新

--- static/js/start_screen.js
+++ static/js/start_screen.js
@@ -1,41 +1,21 @@
-var para=document.createElement("p");
-var node=document.createTextNode("简介简介简介");
-para.appendChild(node);
-var element=document.getElementById("screen_text");
-element.appendChild(para);
+document.getElementById("screen_text").innerHTML="<p>简介简介简介</p>";
 
 document.getElementById("section-1").onclick=function () {change_sc1()};
 function change_sc1() {
-    var para=document.createElement("p");
-    var node=document.createTextNode("简介简介简介");
-    para.appendChild(node);
-    var element=document.getElementById("screen_text");
-    element.appendChild(para);
+    document.getElementById("screen_text").innerHTML="<p>简介简介简介</p>";
 };
 
 document.getElementById("section-2").onclick=function () {change_sc2()};
 function change_sc2() {
-    var para=document.createElement("p");
-    var node=document.createTextNode("安装安装安装");
-    para.appendChild(node);
-    var element=document.getElementById("screen_text");
-    element.appendChild(para);
+    document.getElementById("screen_text").innerHTML="<p>安装安装安装</p>";
 };
 
-document.getElementById("section-2").onclick=function () {change_sc2()};
-function change_sc2() {
-    var para=document.createElement("p");
-    var node=document.createTextNode("JSON配置JSON配置JSON配置");
-    para.appendChild(node);
-    var element=document.getElementById("screen_text");
-    element.appendChild(para);
+document.getElementById("section-3").onclick=function () {change_sc3()};
+function change_sc3() {
+    document.getElementById("screen_text").innerHTML="<p>JSON配置JSON配置JSON配置</p>";
 };
 
-document.getElementById("section-2").onclick=function () {change_sc2()};
-function change_sc2() {
-    var para=document.createElement("p");
-    var node=document.createTextNode("MongoDB配置MongoDB配置MongoDB配置");
-    para.appendChild(node);
-    var element=document.getElementById("screen_text");
-    element.appendChild(para);
+document.getElementById("section-4").onclick=function () {change_sc4()};
+function change_sc4() {
+    document.getElementById("screen_text").innerHTML="<p>MongoDB配置MongoDB配置MongoDB配置</p>";
 };
