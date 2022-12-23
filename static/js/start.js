22d6c8c75366f24cf5b8a94c3f66f387abc7d4b1
Chens <1836178267@qq.com>
2022-12-23 10:51:35 +0800

    更新

--- static/js/start.js
+++ static/js/start.js
@@ -1,4 +1,4 @@
-document.getElementById("text_iframe").src-"https://cnchens.github.io/ChensBOT/templates/startt.html"
+document.getElementById("text_iframe").src="https://cnchens.github.io/ChensBOT/templates/startt.html"
 
 document.getElementById("section-1").onclick=function () {change_sc1()};
 function change_sc1() {
