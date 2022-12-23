0c842319d9b5a732895573fd58997087268ca71d
Chens <1836178267@qq.com>
2022-12-23 10:38:26 +0800

    更新

--- static/js/start.js
+++ static/js/start.js
@@ -1,9 +1,11 @@
+document.getElementById("text_iframe").src-"https://cnchens.github.io/ChensBOT/templates/startt.html"
+
 document.getElementById("section-1").onclick=function () {change_sc1()};
 function change_sc1() {
-    document.getElementById("text_iframe").style.src="https://cnchens.github.io/ChensBOT/templates/startt.html"
+    document.getElementById("text_iframe").src="https://cnchens.github.io/ChensBOT/templates/startt.html"
 }
 
 document.getElementById("section-2").onclick=function () {change_sc2()};
 function change_sc2() {
-    document.getElementById("text_iframe").style.src="https://cnchens.github.io/ChensBOT/templates/test.html"
+    document.getElementById("text_iframe").src="https://cnchens.github.io/ChensBOT/templates/test.html"
 }
