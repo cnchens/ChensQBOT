9ce994c991ad9f7a3e2424a0beac18006010b516
Chens <1836178267@qq.com>
2022-12-23 11:34:42 +0800

    更新

--- static/js/start.js
+++ static/js/start.js
@@ -1,11 +1,41 @@
-document.getElementById("text_iframe").src="https://cnchens.github.io/ChensBOT/templates/startt.html"
+var para=document.createElement("p");
+var node=document.createTextNode("简介简介简介");
+para.appendChild(node);
+var element=document.getElementById("screen_text");
+element.appendChild(para);
 
 document.getElementById("section-1").onclick=function () {change_sc1()};
 function change_sc1() {
-    document.getElementById("text_iframe").src="https://cnchens.github.io/ChensBOT/templates/startt.html"
-}
+    var para=document.createElement("p");
+    var node=document.createTextNode("简介简介简介");
+    para.appendChild(node);
+    var element=document.getElementById("screen_text");
+    element.appendChild(para);
+};
 
 document.getElementById("section-2").onclick=function () {change_sc2()};
 function change_sc2() {
-    document.getElementById("text_iframe").src="https://cnchens.github.io/ChensBOT/templates/test.html"
-}
+    var para=document.createElement("p");
+    var node=document.createTextNode("安装安装安装");
+    para.appendChild(node);
+    var element=document.getElementById("screen_text");
+    element.appendChild(para);
+};
+
+document.getElementById("section-2").onclick=function () {change_sc2()};
+function change_sc2() {
+    var para=document.createElement("p");
+    var node=document.createTextNode("JSON配置JSON配置JSON配置");
+    para.appendChild(node);
+    var element=document.getElementById("screen_text");
+    element.appendChild(para);
+};
+
+document.getElementById("section-2").onclick=function () {change_sc2()};
+function change_sc2() {
+    var para=document.createElement("p");
+    var node=document.createTextNode("MongoDB配置MongoDB配置MongoDB配置");
+    para.appendChild(node);
+    var element=document.getElementById("screen_text");
+    element.appendChild(para);
+};
