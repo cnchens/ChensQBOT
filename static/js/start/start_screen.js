d126a23aafa8bd96aabe9dda3b11725e60667aa1
Chens <1836178267@qq.com>
2022-12-24 17:58:33 +0800

    更新

--- static/js/start/start_screen.js
+++ static/js/start/start_screen.js
@@ -13,7 +13,68 @@ var inner_s2=`
 `
 
 var inner_s3=`
-inner_s3
+<h2>JSON配置</h2>
+<hr>
+<p>本小节只会介绍部分JSON配置的方法，完整版请至<a href="https://cnchens.github.io/ChensBOT/templates/advanced.html">进阶</a>查看</p>
+<p>打开config.json（在根目录中），有以下几行代码需要特别注意：</p>
+<pre class="prettyprint linenums">
+    "mdb_conn" : "mongodb://127.0.0.1:27017/", 
+
+    "superusers" : [114514], 
+    "cmd_start" : [""], 
+    "host" : "127.0.0.1", 
+    "port" : "32808", 
+    "api_root" : "http://127.0.0.1:32808", 
+</pre>
+<p>这几行的配置方法在下表中已经列出来了</p>
+<table class="table table-dark table-striped">
+    <thead>
+        <tr>
+            <th>键名称</th>
+            <th>值类型</th>
+            <th>值默认数据</th>
+            <th>备注</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>mdb_conn</td>
+            <td>str</td>
+            <td>mongodb://127.0.0.1:27017/</td>
+            <td>MongoDB连接url</td>
+        </tr>
+        <tr>
+            <td>superusers</td>
+            <td>list</td>
+            <td>空</td>
+            <td>超级用户</td>
+        </tr>
+        <tr>
+            <td>cmd_start</td>
+            <td>list</td>
+            <td>空</td>
+            <td>命令起始字符</td>
+        </tr>
+        <tr>
+            <td>host</td>
+            <td>str</td>
+            <td>127.0.0.1</td>
+            <td>go-cqhttp连接ip</td>
+        </tr>
+        <tr>
+            <td>port</td>
+            <td>str</td>
+            <td>32808</td>
+            <td>go-cqhttp连接端口</td>
+        </tr>
+        <tr>
+            <td>api_root</td>
+            <td>str</td>
+            <td>http://127.0.0.1:32809</td>
+            <td>nonebot_api连接url</td>
+        </tr>
+    </tbody>
+</table>
 `
 
 var inner_s4=`
