28a7014a92bac6f29d13fdd0fe7abfa6b53067f5
Chens <1836178267@qq.com>
2022-12-25 22:45:36 +0800

    更新

--- static/js/to_users/to_users_screen.js
+++ static/js/to_users/to_users_screen.js
@@ -10,12 +10,14 @@ inner_s2=`
 <hr>
 <p>根据nonebot api，指令的发送可以分为以下两种情形</p>
 <strong>私聊界面：[指令开始字符] 指令内容</strong>
+<br>
 <strong>群聊界面：@[机器人名称] [指令开始字符] 指令内容</strong>
+<br>
 <p>为了适应大众的需求，以下演示将在群聊中进行（私聊指令去除前面的at字段即可，是否需要添加指令开始字符需参考config.json）</p>
 <p>如果没有配置指令开始字符（默认配置为空），就可以直接忽略，示例：群聊界面：@[机器人名称] 指令内容</p>
 <div class="alert alert-danger alert_div">
     <strong>以下是本章中出现的一些特殊字符（如你已经掌握，可以跳过）</strong>
-    <p>cq码的使用，请至<a href="https://docs.go-cqhttp.org/cqcode/#%E8%BD%AC%E4%B9%89">go-cqhttp 帮助中心 -&gt; CQ 码 / CQ Code</a>查看</p>
+    <p>cq码的使用，请至<a href="https://docs.go-cqhttp.org/cqcode/#%E8%BD%AC%E4%B9%89" target="_blank">go-cqhttp 帮助中心 -&gt; CQ 码 / CQ Code</a>查看</p>
     <p>Python常见转义字符：\n 代表换行符；\t 代表横向跳格；\\ 代表反斜杠；\" 代表双引号；\' 代表单引号；\r代表回车；\b代表退格</p>
     <p>pytz日期转义字符：%Y = 年， %m = 月， %d = 日， %H = 时， %M = 分， %S = 秒</p>
     <p>ChensBOT自定义变量：req_qid = 执行本命令的用户QQ号</p>
@@ -274,7 +276,7 @@ inner_s8=`
 inner_s9=`
 <h2>rdsimg</h2>
 <hr>
-<img src="https://cnchens.github.io/ChensBOT/static/image/to_users/allcmd.png" class="img-fluid" alt="allcmd.png">
+<img src="https://cnchens.github.io/ChensBOT/static/image/to_users/rdsimg.png" class="img-fluid" alt="rdsimg.png">
 <br>
 <h5>群聊指令：@[机器人名称] [指令开始字符] rdsimg</h5>
 <br>
